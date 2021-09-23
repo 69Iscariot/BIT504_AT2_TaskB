e8fac917483ce2d6130033836f32faeacb5e9186
69Iscariot <rtemead@gmail.com>
2021-09-23 19:30:56 +1200

    Update Board.java
    Increased the gridness of the grid

--- Board.java
+++ Board.java
@@ -2,7 +2,8 @@ import java.awt.*;
 
 public class Board {
 	// grid line width
-	public static final int GRID_WIDTH = 8;
+	// makes the grid gridier
+	public static final int GRID_WIDTH = 10;
 	// grid line half width
 	public static final int GRID_WIDTH_HALF = GRID_WIDTH / 2;
 	
